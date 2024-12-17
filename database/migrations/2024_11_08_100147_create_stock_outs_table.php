c332ebdef02e6201aaf692842f82a4af8599ef8a
RuslanSenani <abbasovruslan214@gmail.com>
2024-12-17 23:31:07 +0400

    StockOut Migration duzeldildi

--- database/migrations/2024_11_08_100147_create_stock_outs_table.php
+++ database/migrations/2024_11_08_100147_create_stock_outs_table.php
@@ -18,17 +18,17 @@ return new class extends Migration {
         if (!Schema::hasTable('stock_outs')) {
             Schema::create('stock_outs', function (Blueprint $table) {
                 $table->id();
-                $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
-                $table->foreignId('customer_id')->constrained()->onDelete('cascade');
                 $table->foreignId('stock_in_id')->constrained('stock_ins')->onDelete('cascade');
                 $table->foreignId('warehouse_id')->constrained('warehouses')->onDelete('cascade');
+                $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+                $table->string("model_name", 100);
+                $table->string("category_name", 100);
                 $table->string("product_name", 100);
                 $table->string("product_description", 255)->nullable();
                 $table->string("product_code", 100);
-                $table->string("product_category", 100);
                 $table->integer("product_exit_count");
                 $table->string("product_unit", 20);
-                $table->double("product-unit_sale_price");
+                $table->double("product_unit_sale_price");
                 $table->date('exit_date');
                 $this->addCommonColumns($table);
             });
