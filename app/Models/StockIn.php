c5a6b4f7b37f531c1b303b11788060fecb8937e8
RuslanSenani <abbasovruslan214@gmail.com>
2024-12-28 15:17:43 +0400

    bug fix

--- app/Models/StockIn.php
+++ app/Models/StockIn.php
@@ -31,7 +31,7 @@ class StockIn extends Model
 
     public function product(): BelongsTo
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class)->withTrashed();
     }
 
     public function warehouse(): BelongsTo
