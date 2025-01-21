5f34f49be463937b25af99a2a89437eb9d0a8580
RuslanSenani <abbasovruslan214@gmail.com>
2025-01-21 14:45:35 +0400

    js xetalari duzeldilir

--- public/assets/custom/js/custom.js
+++ public/assets/custom/js/custom.js
@@ -1,40 +1,26 @@
-// document.getElementById('delete-button').addEventListener('click', function (event) {
-//     event.preventDefault();
-//     Swal.fire({
-//         title: 'Əminsiz?',
-//         text: "Bu Proses Geri Qaytarılmır!",
-//         icon: 'warning',
-//         showCancelButton: true,
-//         confirmButtonText: 'Bəli, Sil!',
-//         cancelButtonText: 'Xeyir, Ləğv et',
-//         reverseButtons: true
-//     }).then((result) => {
-//         if (result.isConfirmed) {
-//             document.getElementById('delete-form').submit();
-//         }
-//     })
-// })
+$(document).ready(function () {
+    $(function () {
+        //Initialize Select2 Elements
+        $('.select2').select2()
 
-
-$(function () {
-    //Initialize Select2 Elements
-    $('.select2').select2()
-
-    //Initialize Select2 Elements
-    $('.select2bs4').select2({
-        theme: 'bootstrap4'
+        //Initialize Select2 Elements
+        $('.select2bs4').select2({
+            theme: 'bootstrap4'
+        })
     })
-})
 
-$(function () {
-    const fields = ['#about_us', '#address', '#mission', '#vision'];
-    fields.forEach(function (selector) {
-        $(selector).summernote({
-            height: '300px',
-            width: '100%',
+    $(function () {
+        const fields = ['#about_us', '#address', '#mission', '#vision'];
+        fields.forEach(function (selector) {
+            $(selector).summernote({
+                height: '300px',
+                width: '100%',
+            });
         });
     });
-});
+})
+
+
 
 
 
