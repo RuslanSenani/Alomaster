9376639d581187e5b4ead7e8ed50f9f3b511e5a5
RuslanSenani <abbasovruslan214@gmail.com>
2025-01-21 14:09:29 +0400

    js xetalari duzeldilir

--- public/assets/custom/js/custom.js
+++ public/assets/custom/js/custom.js
@@ -15,6 +15,17 @@
 //     })
 // })
 
+
+$(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Initialize Select2 Elements
+    $('.select2bs4').select2({
+        theme: 'bootstrap4'
+    })
+})
+
 $(function () {
     const fields = ['#about_us', '#address', '#mission', '#vision'];
     fields.forEach(function (selector) {
--- resources/views/Back/Users_v/list/content.blade.php
+++ resources/views/Back/Users_v/list/content.blade.php
@@ -78,16 +78,5 @@
             <!-- /.row (main row) -->
         </div><!-- /.container-fluid -->
     </section>
-    <script>
-        $(function () {
-            //Initialize Select2 Elements
-            $('.select2').select2()
-
-            //Initialize Select2 Elements
-            $('.select2bs4').select2({
-                theme: 'bootstrap4'
-            })
-        })
-    </script>
     <!-- /.content -->
 </div>
