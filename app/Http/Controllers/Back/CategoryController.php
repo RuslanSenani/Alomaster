5c1e0c9f31f913575e96fcb1340262559a802634
RuslanSenani <abbasovruslan214@gmail.com>
2025-01-27 13:59:14 +0400

    category controller-de siralam duzeldildi

--- app/Http/Controllers/Back/CategoryController.php
+++ app/Http/Controllers/Back/CategoryController.php
@@ -29,7 +29,7 @@ class CategoryController extends Controller
 
     public function index()
     {
-        $categoryList = $this->category::all();
+        $categoryList = $this->category::orderBy("id", "desc")->get()->all();
 
         $viewData = [
             'viewFolder' => $this->viewFolder,
